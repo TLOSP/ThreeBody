19bfaa14e32bdb4928a76a904a8432b28573d75f
TLOSP <dcf14@software.nju.edu.cn>
2016-01-15 11:45:51 +0800

    项目创建
    使用maven

---
+++ src/main/java/V1/ThreeBody/App.java
@@ -0,0 +1,13 @@
+package V1.ThreeBody;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
